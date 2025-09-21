15ec5c5ae1c408e9cd866879dd3a20a225d6e4ed
cookie4566 <kennedyc416@gmail.com>
2025-09-21 11:41:52 -0400

    version 0.1

---
+++ get_ip/ip_finder.py
@@ -0,0 +1,27 @@
+# IP finder! 
+# use this program to find an IP of any website
+#version 0.1
+
+import socket
+import sys
+
+# socket creation
+try:
+     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     print("socket creation successful")
+except socket.error as err:
+     print("socket creation failed with error %s" %(err))
+# default socket port
+port = 80
+
+try:
+    host_ip = socket.gethostbyname('www.google.com')
+except socket.gaierror:
+    
+    print ("error resolving to host")
+    sys.exit
+    
+s.connect((host_ip, port))
+
+print("the socket has successfully connected!")
+print(host_ip)
